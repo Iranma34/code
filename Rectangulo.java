efd52efde467e4d0a7d1f3efb4c6a8789484b2c3
Emilio Devesa <emilio@MacBook-Pro.local>
2024-07-10 12:26:11 +0200

    Bucles refactorizados en main()

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,31 +1,14 @@
 public class Rectangulo{
 	public static void main(String args[]){
 
-		for(int i=1; i<9; i++){
-			System.out.print("-");
+		// Bucle de la altura
+		for (int altura=0; altura<5; altura++){
+			// Bucle de la anchura
+			for(int i=1; i<9; i++){
+				System.out.print("-");
+			}
+			System.out.println();
 		}
-		System.out.println();
-
-		for(int i=1; i<9; i++){
-			System.out.print("-");
-		}
-		System.out.println();
-
-		for(int i=1; i<9; i++){
-			System.out.print("-");
-		}
-		System.out.println();
-
-		for(int i=1; i<9; i++){
-			System.out.print("-");
-		}
-		System.out.println();
-
-		for(int i=1; i<9; i++){
-			System.out.print("-");
-		}
-		System.out.println();
-
 
 	}
 }
