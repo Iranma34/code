b5d0aa5a7e596714eed29a5babe71fb9589f4b38
Iranma <Iranma@VAIO>
2024-07-10 12:50:18 +0200

    El usuario elige como hacer el rectangulo

--- Rectangulo.java
+++ Rectangulo.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 public class Rectangulo{
 	public static void main(String args[]){
+		// Vamos a pedirle al usuario que introduzca los caracteres manualmente
+		Scanner kb=new Scanner(System.in);
+		System.out.print("Introducir un caracter: ");
+		char perimetro=kb.nextLine().charAt(0);
 
+		// Dibujamos el rectángulo
 		// Bucle de la altura
 		for (int altura=0; altura<5; altura++){
 			// Bucle de la anchura
 			for(int i=1; i<9; i++){
-				System.out.print("-");
+				System.out.print(perimetro);
 			}
 			System.out.println();
 		}
 
+
+		kb.close();
+
 	}
+
 }
+
