46e29bb99849c5ef1ee4eafb8c09e6dc900520a5
Iranma <Iranma@VAIO>
2024-07-10 13:11:32 +0200

    Se pinta un triangulo el pantalla

---
+++ Triangulo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Triangulo{
+
+	public static void main(String args[]){
+		Scanner kb=new Scanner(System.in);
+
+		System.out.println("-");
+		System.out.println("--");
+		System.out.println("---");
+		System.out.println("----");
+
+		
+
+
+		kb.close();
+
+	}
+
+}
+
