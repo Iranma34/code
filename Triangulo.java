17a727f86df029a9240ec5dcdd577bbcce9697ca
Iranma <Iranma@VAIO>
2024-07-10 13:22:38 +0200

    Refactorización del triángulo

--- Triangulo.java
+++ Triangulo.java
@@ -6,13 +6,14 @@ public class Triangulo{
 	public static void main(String args[]){
 		Scanner kb=new Scanner(System.in);
 
-		System.out.println("-");
-		System.out.println("--");
-		System.out.println("---");
-		System.out.println("----");
-
-		
-
+		// Bucle de la altura del triangulo
+		for (int i=0; i<4; i++){
+			// Bucle de la anchura de cada línea (depende del número de linea i)
+			for(int k=0; k<=i; k++){
+				System.out.print("-");
+			}
+			System.out.println();
+		}
 
 		kb.close();
 
