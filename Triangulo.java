20fb4a82dba3a6fc6c5884152e49ce9c0aab9850
Iranma <Iranma@VAIO>
2024-07-10 13:34:57 +0200

    El usuario elige la altura y el caracter

--- Triangulo.java
+++ Triangulo.java
@@ -6,11 +6,19 @@ public class Triangulo{
 	public static void main(String args[]){
 		Scanner kb=new Scanner(System.in);
 
+		// Pedimos las dimensiones del triangulo
+		System.out.print("Introducir altura: ");
+		int altura=kb.nextInt();
+		kb.nextLine();
+
+		System.out.print("Introduzca un caracter: ");
+		char contenido=kb.nextLine().charAt(0);
+
 		// Bucle de la altura del triangulo
-		for (int i=0; i<4; i++){
+		for (int i=0; i<altura; i++){
 			// Bucle de la anchura de cada línea (depende del número de linea i)
 			for(int k=0; k<=i; k++){
-				System.out.print("-");
+				System.out.print(contenido);
 			}
 			System.out.println();
 		}
