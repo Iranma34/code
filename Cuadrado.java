b13f4c44b85f5c830baccfe2d44b8ec728efd107
Emilio Devesa <emilio@MacBook-Pro.local>
2024-07-11 00:53:56 +0200

    He corregido el programa y añadido comentarios al código

--- Cuadrado.java
+++ Cuadrado.java
@@ -3,33 +3,44 @@ import java.util.Scanner;
 public class Cuadrado{
 
 	public static void main(String[] args){
-	Scanner kb=new Scanner(System.in);
+		Scanner kb=new Scanner(System.in);
 
-	System.out.print("Introducir lado: ");
-	int lado=kb.nextInt();
-	kb.nextLine();
-	
-	System.out.print("Introducir ladoDos: ");
-	int ladoDos=kb.nextInt();
-	kb.nextLine();
+		/**
+		 * Pedimos el lado del cuadrado al usuario
+		 * */
+		System.out.print("Introducir lado: ");
+		int lado=kb.nextInt();
+		kb.nextLine();
+		
+		/**
+		 * No necesito preguntar dos datos, ya que
+		 * en el cuadrado todos los lados deben ser iguales
+		 * 
+		 * System.out.print("Introducir ladoDos: ");
+		 * int ladoDos=kb.nextInt();
+		 * kb.nextLine();
+		 * */
 
-	System.out.print("Introduzca un caracter: ");
-	char forma=kb.nextLine().charAt(0);
+		/**
+		 * Pedimos el caracter con el que pintar el cuadrado
+		 * */
+		System.out.print("Introduzca un caracter: ");
+		char forma=kb.nextLine().charAt(0);
 
-	for(int i=0; i<lado; i++){
-
-		for(int k=0; k<=ladoDos; k++){
-			System.out.print(forma);
+		/**
+		 * Este bucle lleva cuenta de la altura (altura = lado)
+		 * */
+		for(int i=0; i<lado; i++){
+			/**
+			 * Este bucle lleva cuenta del ancho (ancho = lado)
+			 * */
+			for(int k=0; k<lado; k++){
+				System.out.print(forma);
+			}
+			System.out.println();
 		}
-		System.out.println();
+		
+		kb.close();
 	}
-
-
-
-
-kb.close();
-	}
-	
-
 
 }
