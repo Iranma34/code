e943d0caf87a79c40028b258ee7ed2599e0fe0c3
Emilio Devesa <emilio@MacBook-Pro.local>
2024-07-11 12:42:08 +0200

    Mostramos el menu con una sola instruccion print()

--- Menu.java
+++ Menu.java
@@ -18,16 +18,16 @@ public class Menu {
 		int opcion=0;
 		do {
 			// Mostrar el menú
-			System.out.println();
-			System.out.println("MENU PRINCIPAL");
-			System.out.println("1) Contar palabras");
-			System.out.println("2) Mostrar cada palabra en una línea");
-			System.out.println("3) Salir");
-			System.out.print("Opcion: ");
+			System.out.print("""
+			MENU PRINCIPAL
+			1) Contar palabras
+			2) Mostrar cada palabra en una línea
+			3) Salir
+			Opcion: """);
 			opcion = kb.nextInt();
 			kb.nextLine();
 
-			if (opcion== 1){
+			if (opcion == 1){
 				// Contar el número de palabras en esa línea de texto
 				int numPalabras=0;
 				for (int i = 0; i < texto.length(); i++){
