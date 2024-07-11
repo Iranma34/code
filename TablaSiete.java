3d848e7a2829ea24b9f5a45ac7e0c7ee33e2f1e9
Iranma <Iranma@VAIO>
2024-07-11 16:20:49 +0200

    Vamos a hacer una tabla del 7

---
+++ TablaSiete.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TablaSiete {
+
+	public static void main(String[] args){
+		Scanner kb=new Scanner(System.in);
+
+		System.out.print("Tabla del siete: ");
+		int numeroEntero=teclado.nextInt();
+
+		kb.close();
+
+
+	}
+
+}
