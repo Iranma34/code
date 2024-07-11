5622f06fdf0c6403a0bf7ab373ac921a28932fbd
Iranma <Iranma@VAIO>
2024-07-11 13:36:11 +0200

    El programa pregunta la edad del usuario

--- MayorDeEdad.java
+++ MayorDeEdad.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
 
+/**
+ * Este programa pregunta al usuario su edad
+ * y muestra un mensaje diciendo si es o no
+ * mayor de edad
+ * */
 public class MayorDeEdad {
 
 	public static void main(String[] args){
 		Scanner kb=new Scanner(System.in);
+
+		System.out.print("¿Cuántos años tiene?: ");
+		int edad=kb.nextInt();
 
 		kb.close();
 	}
