b190dbcfd16f38f4f506bf04e5c5b714cfafa73a
Iranma <Iranma@VAIO>
2024-07-11 17:03:13 +0200

    Hemos hecho una tabla del 7

--- TablaSiete.java
+++ TablaSiete.java
@@ -1,16 +1,19 @@
-import java.util.Scanner;
+
 
 public class TablaSiete {
 
 	public static void main(String[] args){
-		Scanner kb=new Scanner(System.in);
-
+		
 		System.out.print("Tabla del siete: ");
-		int numeroEntero=teclado.nextInt();
 
-		kb.close();
+		
+        
+		for(int i=0; i<=10; i++){
+			System.out.println("7*"+i+"="+7*i);
+		}
+			
+			
 
-
-	}
+	}	
 
 }
