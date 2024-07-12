9d2ac7a2961e029d8bbceecf89dd1d68f7686fd7
Iranma <Iranma@VAIO>
2024-07-12 13:28:45 +0200

    Commit inicial, respuesta si/no

---
+++ RespuestaSiNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+//Este programa pregunta al usuario si o no y muestra su respuesta por pantalla
+//Para responder si tecleará una s y una n en caso negativo
+//En cualquier otro caso, el programa vuelve a hacer la pregunta
+public class RespuestaSiNo{
+	
+	public static void main(String[] args){
+		Scanner kb=new Scanner(System.in);
+
+
+	
+		kb.close();
+	}
+}
