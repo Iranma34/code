abc536c707e2e569d4a0d2c1d63fe2ce39c8428f
Iranma <Iranma@VAIO>
2024-07-12 13:18:24 +0200

    La lógica del programa está corregida y terminada y el código limpio y ordenado

--- Tratamiento.java
+++ Tratamiento.java
@@ -5,82 +5,31 @@ public class Tratamiento{
 	public static void main(String[]args){
 		Scanner kb=new Scanner(System.in);
 
-        //Preguntamos si el usuario es mayor de edad
-		System.out.println("Edad? : ");
-		
+		System.out.print("Edad?: ");
 		int edad=kb.nextInt();
 		kb.nextLine();
 
-		if (edad<18){
-			System.out.println("Menor de edad");
-		} else {
-			System.out.println("Mayor de edad");
-		}
-
-
-		System.out.println("Sexo?: ");
-		
+		System.out.print("Sexo?: ");
 		char sexo=kb.nextLine().charAt(0);
-		kb.nextLine();
-
-		if(sexo=='f'){
-
-		} else {
-			System.out.println("F");
-		} 
-		if(edad<18){
-			System.out.printl("Señorita");
-
-		} else {
-			System.out.printl("Señora");
-		}
-
-		if(sexo=='m'){
-
-		}else {
-			System.out.println("M");
-		}
-		if(edad<18){
-			System.out.printl("Señorito");
-
-		} else {
-			System.out.printl("Señor");
-		}
-
-
-		/*if((sexo=='f') || (sexo=='m')){
-			System.out.print("Señor");
 		
-		} else {
-			System.out.println("Señorito");
-		}*/
+		if((sexo=='f') || (sexo=='F')){
+			if(edad<18){
+				System.out.println("Señorita");
+			} else {
+				System.out.println("Señora");
+			}
+		}
 
+		if((sexo=='m') || (sexo=='M')) {
+			if(edad<18){
+				System.out.println("Señorito");
+			} else {
+				System.out.println("Señor");
+			}
+		} 
 
 		kb.close();
 
-
 	}
 
-
 }
-
-	
-		
-
-
-
-
-
-
-	
-	    
-
-    
-    
-
-
-
-
-	
-
-
