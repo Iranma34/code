c834e848f2d70dc7504cbb272fb4b9cdaec87c8a
Emilio Devesa <emilio@w11>
2024-07-12 10:53:23 +0200

    Lógica terminada

--- OrdenarNumeros.java
+++ OrdenarNumeros.java
@@ -12,32 +12,41 @@ public class OrdenarNumeros{
         System.out.print("Introduce el tercer numero: ");
         int c=kb.nextInt();
 
-        //Asumimos que el primer número es el menor
-        
-        
+        //Asumimos que el primer número es el menor        
         if(b<a){
+    		// B es menor que A
         	if(c<b){
-        		System.out.println(c+" "b+" "a);
-
+        		// C es menor que B
+        		System.out.println(c+" "+b+" "+a);
         	}else{
-        		if(c<a){
-        			System.out.println(b+" "+c+" "+a);
-        		}
-
-        	}
+				// C es mayor que B
+	    		if(c<a){
+	    			// C es menor que A
+	    			System.out.println(b+" "+c+" "+a);
+	    		} else {
+	    			// C es mayor que A
+	    			System.out.println(b+" "+a+" "+c);
+	    		}
+	        }
         }else{
+        	// B es mayor que A
         	if(c<a){
-        		System.out.println(a+" "+b+" "+c);
+        		// C es menor que A
+        		System.out.println(c+" "+a+" "+b);
+        	} else {
+        		// C es mayor que A
+        		if(c<b){
+        			// C es menor que B
+        			System.out.println(a+" "+c+" "+b);
+        		} else {
+        			// C es mayor que B
+        			System.out.println(a+" "+b+" "+c);
+        		}
         	}
         }
-        
-       
-        
-		
-
 		
 		//Mostramos el resultado
-		System.out.print("El orden es: ");
+		//System.out.print("El orden es: ");
 		
 		kb.close();
 		}	
