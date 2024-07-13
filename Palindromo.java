0b3f6ad056f4927c5f6bb17fb10726de3100055d
Emilio Devesa <emilio@ubuntu-vm>
2024-07-13 08:56:22 +0200

    [Palindromo.java] Initial commit: enunciado del problema

---
+++ Palindromo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Este programa lee un text introducido por el usuario
+ * A continuacion lo escribe en pantalla con todos los caracteres en orden inverso
+ * Si por ejemplo el usuario introduce la palabra "Hotel"
+ * El programa escribirá:
+ * letoh
+ * */
+public class Palindromo{
+	
+	public static void main(String[] args){
+
+	}
+}
