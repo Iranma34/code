62276ac773b0913a493ba9fb921d09ea383d6c11
Emilio Devesa <emilio@ubuntu-vm>
2024-07-13 09:00:39 +0200

    [Palindromo.java] codigo de main() y leerTexto()

--- Palindromo.java
+++ Palindromo.java
@@ -8,8 +8,24 @@ import java.util.Scanner;
  * letoh
  * */
 public class Palindromo{
+
+	Scanner sc=new Scanner(System.in);
 	
 	public static void main(String[] args){
+		String txt=leerTexto();
+		escribirPalindromo(txt);
+	}
 
+	// Este método no recibe ningun parámetro
+	// Devuelve un 'String'
+	public static String leerTexto(){
+		String txt=sc.nextLine();
+		return txt;
+	}
+
+	// Este método recibe como parámetro un 'String'
+	// No devuelve nada 'void'
+	public static void escribirPalindromo(String texto){
+		// Completar codigo del método
 	}
 }
