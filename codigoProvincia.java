102b2aaa4f54c20b808ce99e174e7d3edab6eeb8
Emilio Devesa <emilio@ubuntu-vm>
2024-07-13 08:44:34 +0200

    [codigoProvincia.java] Commit inicial: codigo de main() y leerCodigoProvincia()

--- codigoProvincia.java
+++ codigoProvincia.java
@@ -16,7 +16,23 @@ public class codigoProvincia{
 	static Scanner sc=new Scanner(System.in);
 
 	public static void main(String[] args){
+		int codigo = leerCodigoProvincia();
+		comprobarCodigoProvincia(codigo);
+	}
 
+	// Este método no recibe ningún parámetro
+	// Devuelve un 'int'
+	public static int leerCodigoProvincia(){
+		System.out.print("Introducir codigo: ");
+		int codigo=sc.nextInt();
+		sc.nextLine();
+		return codigo;
+	}
+
+	// Este método recibe como parámetro un código
+	// No devuelve nada 'void'
+	public static void comprobarCodigoProvincia(int codigo){
+		// Completar el programa escribiendo este método
 	}
 
 }
