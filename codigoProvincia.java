7bf28499730653de4fd6222006d7cb2cd3bc8840
Emilio Devesa <emilio@ubuntu-vm>
2024-07-13 08:38:32 +0200

    [codigoProvincia.java] Commit inicial: enunciado del problema

---
+++ codigoProvincia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * Este programa pregunta al usuario un código
+ * Si se corresponde con una provincia gallega, muestra el nombre
+ * En caso contrario, responde el mensaje "Código desconocido"
+ * Los códigos son:
+ * 		15: A Coruña
+ * 		27: Lugo
+ *		32: Ourense
+ * 		38: Pontevedra 		
+ * */
+public class codigoProvincia{
+	
+	static Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args){
+
+	}
+
+}
