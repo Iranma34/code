861f609fee7ae08669bb93a221c0ed69d0fb6c7f
Iranma <Iranma@VAIO>
2024-07-14 10:09:17 +0200

    El programa muestra errores al compilar y ejecutar

--- codigoProvincia.java
+++ codigoProvincia.java
@@ -16,11 +16,32 @@ public class CodigoProvincia{
 	static Scanner sc=new Scanner(System.in);
 
 	public static void main(String[] args){
-		int codigo = leerCodigoProvincia();
-		comprobarCodigoProvincia(codigo);
-	}
+		System.out.print("¿Cuál es el código?: ");
+		int codigoProvincia=sc.nextInt();
 
-	// Este método no recibe ningún parámetro
+		public static boolean getConfirmation(){
+
+		if(codigoProvincia==15){
+			System.out.println("A Coruña");
+			{ else 
+		if(codigoProvicia==27){
+			System.out.println("Lugo");
+			{ else
+		if(codigoProvincia==32){
+			System.out.println("Ourense");
+			{ else 
+		if(codigoProvincia==38){
+			System.out.println("Pontevedra");
+			} else {
+			System.out.println("Código desconocido");
+
+		}
+
+			}
+		}
+
+		
+		/*// Este método no recibe ningún parámetro
 	// Devuelve un 'int'
 	public static int leerCodigoProvincia(){
 		System.out.print("Introducir codigo: ");
@@ -33,6 +54,5 @@ public class CodigoProvincia{
 	// No devuelve nada 'void'
 	public static void comprobarCodigoProvincia(int codigo){
 		// Completar el programa escribiendo este método
-	}
+	}*/
 
-}
