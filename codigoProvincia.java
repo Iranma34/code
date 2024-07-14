b0a41dfd423f80568dca9ab8c346b1fe566af598
Emilio Devesa <emilio@ubuntu-vm>
2024-07-14 11:18:56 +0200

    [CodigoProvincia.java] Corregida indentación, encapsulación en métodos, pequeñas correcciones de errores en llaves de las condiciones

--- codigoProvincia.java
+++ codigoProvincia.java
@@ -16,32 +16,11 @@ public class CodigoProvincia{
 	static Scanner sc=new Scanner(System.in);
 
 	public static void main(String[] args){
-		System.out.print("¿Cuál es el código?: ");
-		int codigoProvincia=sc.nextInt();
+		int codigo=leerCodigoProvincia();
+		comprobarCodigoProvincia(codigo);
+	}
 
-		public static boolean getConfirmation(){
-
-		if(codigoProvincia==15){
-			System.out.println("A Coruña");
-			{ else 
-		if(codigoProvicia==27){
-			System.out.println("Lugo");
-			{ else
-		if(codigoProvincia==32){
-			System.out.println("Ourense");
-			{ else 
-		if(codigoProvincia==38){
-			System.out.println("Pontevedra");
-			} else {
-			System.out.println("Código desconocido");
-
-		}
-
-			}
-		}
-
-		
-		/*// Este método no recibe ningún parámetro
+	// Este método no recibe ningún parámetro
 	// Devuelve un 'int'
 	public static int leerCodigoProvincia(){
 		System.out.print("Introducir codigo: ");
@@ -53,6 +32,23 @@ public class CodigoProvincia{
 	// Este método recibe como parámetro un código
 	// No devuelve nada 'void'
 	public static void comprobarCodigoProvincia(int codigo){
-		// Completar el programa escribiendo este método
-	}*/
+		if(codigo==15){
+			System.out.println("A Coruña");
+		} else {
+			if(codigo==27){
+				System.out.println("Lugo");
+			} else {
+				if(codigo==32){
+					System.out.println("Ourense");
+				} else {
+					if(codigo==38){
+						System.out.println("Pontevedra");
+					} else {
+						System.out.println("Código desconocido");
+					}
+				}
+			}
+		}
+	}
 
+}
