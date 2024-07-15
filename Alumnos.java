f9274e4e89d0b225eeb7460be483d969372d130c
Iranma <Iranma@VAIO>
2024-07-15 12:34:37 +0200

    Programa que sirve como fichero de datos de alumnos

---
+++ Alumnos.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Hacer un programa que muestra un menú con las siguientes opciones:
+ * 1- Introducir datos de un alumno(nombre, edad y teléfono)
+ * 2- Cambiar nombre 
+ * 3- Cambiar edad
+ * 4- Cambiar teléfono
+ * 5- Mostrar datos del alumno 
+ * 0- Salir del programa
+ * */
+
+ public class Alumnos{
+ 	public static void main(String[] args){
+
+
+ 	kb.close();	
+ 	}
+ }
