395f02a374e0317f8712bb5d1a7e77af25440355
Emilio Devesa <emilio@ubuntu-vm>
2024-07-15 14:53:08 +0200

    [Figuras.java] Enunciado del ejercicio

---
+++ Figuras.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Escribir un programa que al empezar muestra un menu con
+ * las siguientes opciones:
+ * 1) Dibujar un cuadrado
+ * 2) Dibujar un triángulo
+ * 0) Salir
+ * 
+ * Cuando el usuario selecciona una de las dos opciones para
+ * pintar una figura, el programa le preguntará el caracter
+ * empleado para dibujarla y las dimensiones de la misma.
+ * 
+ * El código fuente debe estar bien encapsulado en diferentes
+ * métodos
+ * El programa no termina tras dibujar una figura, si no que
+ * vuelve al menu principal y solo terminará cuando el usuario
+ * escoja la opción "Salir"
+ * 
+ */
