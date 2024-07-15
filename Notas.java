2fa22a8781ef0ed84c061b5ad49335a7b4341964
Iranma <Iranma@VAIO>
2024-07-15 10:13:19 +0200

    Programa terminado y corregido

--- Notas.java
+++ Notas.java
@@ -5,40 +5,52 @@ public class Notas{
 	public static void main(String[] args){
 		Scanner kb=new Scanner(System.in);
 
-		Sytem.out.println("Introduce la nota: ");
+		System.out.print("Introduce la nota: ");
 		int nota=kb.nextInt();
 
-		System.out.println("La nota es:"+resultado);
-
+		String evaluacion = "";
+	/*	
 		if(nota== 1 || nota== 2){
-			return "Muy deficiente";
+			evaluacion = "Muy deficiente";
+		} else {
+			if(nota==3 || nota==4){
+				evaluacion = "Deficiente";
 			} else {
-		if(nota==3 || nota==4){
-			return "Deficiente";
-			} else {
-		if(nota== 5){
-			return "Suficiente";
-			} else {
-		if(nota== 6){
-			return"Bien";
-			} else {
-		if(nota==7 || nota==8){
-			return"Notable";
-			} else {
-		if(nota==9 || nota==10){
-			return"Sobresalliente";
+				if(nota== 5){
+					evaluacion = "Suficiente";
+				} else {
+					if(nota== 6){
+						evaluacion ="Bien";
+					} else {
+						if(nota==7 || nota==8){
+							evaluacion ="Notable";
+						} else {
+							if(nota==9 || nota==10){
+								evaluacion ="Sobresaliente";
+							} else {
+								System.out.println("La nota debe estar comprendida entre 1 y 10");
+							}
+						}
+					}
+				}
+			}
 		}
+	*/
+
+		// Lógica de las comparaciones simplificada
+		if (nota==1 || nota==2) { evaluacion="Muy deficiente"; }
+		if (nota==3 || nota==4) { evaluacion="Deficiente"; }
+		if (nota==5) { evaluacion="Suficiente"; }
+		if (nota==6) { evaluacion="Bien"; }
+		if (nota==7 || nota==8) { evaluacion="Notable"; }
+		if (nota==9 || nota==10) { evaluacion="Sobresaliente"; }
+
+		if (evaluacion == ""){
+			System.out.println("La nota debe estar comprendida entre 1 y 10");
+		} else {
+			System.out.println("La nota es: "+evaluacion);
 		}
-		}
-		}
 
-			
-
-
-		}
-
-
-
-	kb.close();	
+		kb.close();	
+	}
 }
- 	}	
