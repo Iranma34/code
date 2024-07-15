b6ffbd6be50a7146944f1c783f462a79b334194a
Iranma <Iranma@VAIO>
2024-07-15 13:04:53 +0200

    El programa muestra el menú en pantalla

--- Alumnos.java
+++ Alumnos.java
@@ -12,8 +12,18 @@ import java.util.Scanner;
 
  public class Alumnos{
  	public static void main(String[] args){
+ 		System.out.println("Menú principal");
+
+ 		System.out.println("1) Introduzca datos del alumno");
+ 		System.out.println("2) Cambiar nombre");
+ 		System.out.println("3) Cambiar edad");
+ 		System.out.println("4) Cambiar teléfono");
+ 		System.out.println("5) Mostrar datos del alumno");
+ 		System.out.println("0) Salir del programa");
 
 
- 	kb.close();	
+
+
+
  	}
  }
