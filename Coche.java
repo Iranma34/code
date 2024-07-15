a03acbe04736b67ceb09b006b5ceb3e8628ba6ea
Emilio Devesa <emilio@ubuntu-vm>
2024-07-15 14:52:46 +0200

    [Coche.java] Enunciado del ejercicio

---
+++ Coche.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Escribir un programa similar a 'Alumnos.java' en el que
+ * se pueda trabajar con los siguientes datos de un coche:
+ * - Marca
+ * - Modelo
+ * - Año
+ * - Color
+ * - Matrícula
+ * - Estado de revisión (que puede ser Superada o No superada)
+ * 
+ * El programa debe estar dirigido por menús
+ * La primera opción debe servir para introducir todos los
+ * datos de un coche, las siguientes para modificarlos de
+ * forma individual y la última para mostrar todos los datos
+ * del coche (además de una opción de salir)
+ * 
+ * El codigo debe estar encapsulado en diferentes métodos
+ * */
