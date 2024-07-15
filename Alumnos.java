01682ab384c12f9955257072af15f25c27a02022
Iranma <Iranma@VAIO>
2024-07-15 14:02:52 +0200

    El menu se muestra hasta que el usuario decida abandonar el programa

--- Alumnos.java
+++ Alumnos.java
@@ -21,35 +21,39 @@ import java.util.Scanner;
  	static int edad;
 
  	public static void main(String[] args){
- 		System.out.println("Menú principal");
- 		System.out.println("1) Introduzca datos del alumno");
- 		System.out.println("2) Cambiar nombre");
- 		System.out.println("3) Cambiar edad");
- 		System.out.println("4) Cambiar teléfono");
- 		System.out.println("5) Mostrar datos del alumno");
- 		System.out.println("0) Salir del programa");
- 		System.out.print("Selecciona una opcion: ");
- 		int opcion=sc.nextInt();
-		sc.nextLine(); // Vacia el buffer de teclado para evitar que la siguiente lectura arrastre basura previa
- 			
- 		if (opcion==0){
- 			System.out.println("Hasta pronto!");
- 		}
- 		if(opcion==1){
- 			introducirDatos();
- 		}
- 		if(opcion==2){
- 			cambiarNombre();
- 		}
- 		if(opcion==3){
- 			cambiarEdad(); 			
- 		}
- 		if(opcion==4){
- 			cambiarTelefono(); 			
- 		}
- 		if(opcion==5){
- 			mostrarDatosAlumno(); 			
- 		}
+ 		int opcion;
+ 		do {
+			System.out.println("Menú principal");
+	 		System.out.println("1) Introduzca datos del alumno");
+	 		System.out.println("2) Cambiar nombre");
+	 		System.out.println("3) Cambiar edad");
+	 		System.out.println("4) Cambiar teléfono");
+	 		System.out.println("5) Mostrar datos del alumno");
+	 		System.out.println("0) Salir del programa");
+	 		System.out.print("Selecciona una opcion: ");
+	 		opcion=sc.nextInt();
+			sc.nextLine(); // Vacia el buffer de teclado para evitar que la siguiente lectura arrastre basura previa
+	 			
+	 		if (opcion==0){
+	 			System.out.println("Hasta pronto!");
+	 		}
+	 		if(opcion==1){
+	 			introducirDatos();
+	 		}
+	 		if(opcion==2){
+	 			cambiarNombre();
+	 		}
+	 		if(opcion==3){
+	 			cambiarEdad(); 			
+	 		}
+	 		if(opcion==4){
+	 			cambiarTelefono(); 			
+	 		}
+	 		if(opcion==5){
+	 			mostrarDatosAlumno(); 			
+	 		}
+ 		} while (opcion!=0);
+ 		
  	}
 
  	public static void introducirDatos(){
