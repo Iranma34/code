5351452abbb5216c1e0d4d6201e2b180121699b0
Iranma <Iranma@VAIO>
2024-07-18 13:09:57 +0200

    Implementamos un algoritmo sencillo que comprueba si un numero es primo

--- Primos.java
+++ Primos.java
@@ -7,8 +7,27 @@ import java.util.Scanner;
  * */
 
 public class Primos{
+	
+	static Scanner sc=new Scanner(System.in);
+	static int numero;
+	static int contadorDivisores;
+	
 	public static void main(String[] args){
+		System.out.print("Introduzca un número ");
+		numero=sc.nextInt();
+		sc.nextLine();
 		
+		for(int i=1; i<=numero; i++){
+			if (numero % i == 0){
+				contadorDivisores++;
+			}
+		}
+
+		if(contadorDivisores==2){
+			System.out.print("El numero es primo");
+		} else {
+			System.out.print("El numero no es primo");
+		}
 
 	}
 }
