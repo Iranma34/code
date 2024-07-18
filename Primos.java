66b0b8399aae5d46d135673c8698242d43e1cf27
Iranma <Iranma@VAIO>
2024-07-18 12:26:51 +0200

    Programa primos.java

---
+++ Primos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * 
+ * Primos.java
+ * 
+ * Este programa pide al usuario un número y determina si es o no un número primo
+ * */
+
+public class Primos{
+	public static void main(String[] args){
+		
+
+	}
+}
