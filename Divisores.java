7b303c3694fecd5e0a8d00b63ed2b1ad551dc9dc
Iranma <Iranma@VAIO>
2024-07-18 16:08:31 +0200

    Intento programa que escribe los divisores de un número

---
+++ Divisores.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+/**Este programa pide un número entre 1 y 100 y escribe en pantalla los divisores de ese número 
+**/
+public class Divisores{
+	static Scanner sc=new Scanner(System.in);
+	static int a;
+public static void main(String[] args){
+
+
+	System.out.print("Ingrese un número entre 1 y 100");
+		a=sc.nextInt();
+
+	if(a<1 || a>100){
+		System.out.print("El número está fuera del rango.");
+	}
+
+	System.out.print("Los divisores de"+a+ "son: ");
+	for(int i=1; i<=a; i++){
+		if(a % i==0){
+			System.out.print("i +");
+		}
+	}
+}
+	
+}
