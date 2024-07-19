67615099458b035e2ed86ec39ba5b18eab1b230d
Iranma <Iranma@VAIO>
2024-07-19 11:51:16 +0200

    El programa pide un número

---
+++ Fibonacci.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Introduzca un número: ");
+		int numero=sc.nextInt();
+		sc.nextLine();
+
+		sc.close();
+	}
+
+}
