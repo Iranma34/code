da3e5e2631978f5a99f4aeefbee8c5244271589e
Iranma <Iranma@VAIO>
2024-07-19 09:51:15 +0200

    Se muestra el código en métodos separados

--- Saldo.java
+++ Saldo.java
@@ -12,20 +12,51 @@ un mensaje:
 public class Saldo{
 	static Scanner sc=new Scanner(System.in);
 	static int saldo;
-
-public static void main(String[] args){
-	double saldo=0;
-	int opcion;
-	do{
-		System.out.println("1.Ingresar dinero");
-		System.out.println("2.Retirar dinero");
-		System.out.println("3.Consultar saldo");
-		System.out.println("0.Salir");
-		System.out.print("Seleccione una opcion: ");
-		opcion=sc.nextInt();
+	
+	public static void main(String[] args){
+		System.out.print("Saldo inicial? ");
+		saldo=sc.nextInt();
 		sc.nextLine();
-	}while(opcion!=0);
 
-}
-}
+		int opcion;
+		do{
+			System.out.println("1.Ingresar dinero");
+			System.out.println("2.Retirar dinero");
+			System.out.println("3.Consultar saldo");
+			System.out.println("0.Salir");
+			System.out.print("Seleccione una opcion: ");
+			opcion=sc.nextInt();
+			sc.nextLine();
 
+			if(opcion==1){
+				ingresarDinero();
+			}
+			if(opcion==2){
+				retirarDinero();
+			}
+			if(opcion==3){
+				consultarSaldo();
+			}
+		}while(opcion!=0);
+	}
+
+	public static void ingresarDinero(){
+		System.out.print("Cuánto quiere ingresar: ");
+		int ingreso=sc.nextInt();
+		sc.nextLine();
+		saldo=saldo+ingreso;
+	}
+	public static void retirarDinero(){
+		System.out.print("Cuánto quiere retirar: ");
+		int retiro=sc.nextInt();
+		sc.nextLine();
+		if(retiro<=saldo){
+			saldo=saldo-retiro;
+		} else {
+			System.out.println("Saldo insuficiente");
+		}
+	}
+	public static void consultarSaldo(){
+		System.out.println("Su saldo es: "+saldo);
+	}
+}
