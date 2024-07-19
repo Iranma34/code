0742e944dbc8fe79e95322d85f5feac850f4b7e0
Iranma <Iranma@VAIO>
2024-07-19 12:05:42 +0200

    El programa devuelve el número en la posición solicitada

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * Sucesion fibonacci: 1 1 2 3 5 8 13 21 ...
+ * 
+ * Fibonacci(posicion)=numero en esa posicion
+ * Fibonacci(0)=1
+ * Fibonacci(1)=1
+ * Fibonacci(2)=2
+ * Fibonacci(3)=3
+ * Fibonacci(4)=5
+ * Fibonacci(5)=8
+ * Fibonacci(6)=13
+ * 
+ * */
+
 public class Fibonacci{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
@@ -7,7 +21,18 @@ public class Fibonacci{
 		int numero=sc.nextInt();
 		sc.nextLine();
 
+		System.out.println(fibo(numero));
 		sc.close();
+	}
+
+	public static int fibo(int n){
+		if (n<2){
+			return 1;
+		} else {
+			// Llamo al mismo método que estoy programando
+			// Esto se llama RECURSIVIDAD DIRECTA
+			return fibo(n-1)+fibo(n-2);
+		}
 	}
 
 }
